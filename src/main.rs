09573945d3d7ff9f010094604b9eb7a781cc1a46
BugsBound <bugsbound@gmail.com>
2023-05-12 16:44:24 +0200

    Change struct Config

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use std::env;
+use std::{env, process};
 use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Fail with args: {}", err);
+        process::exit(1);
+    });
 
     println!("Req:{} - File:{}", config.query, config.filename);
 
@@ -20,10 +23,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Self {
-        Config {
-            query: args[1].clone(),
-            filename: args[2].clone(),
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("need Query and Filename!");
         }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
     }
 }
