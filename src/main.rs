64398441d3ff9c3be074ee79398f2ad94fa4b532
BugsBound <bugsbound@gmail.com>
2023-05-12 16:30:15 +0200

    add struct Config

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,26 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = args.get(1).unwrap();
-    let filename = &args[2];
+    let config = Config::new(&args);
 
-    println!("T:{} - F:{}", query, filename);
+    println!("Req:{} - File:{}", config.query, config.filename);
+
+    let contents = fs::read_to_string(config.filename)
+        .expect("Something wrong when read file.");
+
+    println!("Text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Self {
+        Config {
+            query: args[1].clone(),
+            filename: args[2].clone(),
+        }
+    }
 }
